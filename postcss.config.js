af57bed22c7ad6f93df55ddbd3fca45816dbbdda
Yusif Jabrayilov <mahirliy@Yusifs-MacBook-Pro.local>
2025-06-21 09:34:28 +0200

    feat: add PostCSS preset env plugin for enhanced CSS features

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-env': {
+      features: {
+        'nesting-rules': true,
+        'custom-media-queries': true,
+        'media-query-ranges': true
+      }
+    }
   },
 } 
